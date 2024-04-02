d7fe8c1af948026af5489334f73d2c7c8137ce3e
Armeth <jwpro1984@gmail.com>
2024-04-02 17:12:11 -0400

    Sidebar/MobNav
    Built sidebar and mobile navigation

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TransformationsPage = () => {
+  return (
+    <div>TransformationsPage</div>
+  )
+}
+
+export default TransformationsPage
+
+//Use Dynamic Routes (check next.js docs)
+//localhost:30000/transformations
+//localhost:30000/transformations/[id]
