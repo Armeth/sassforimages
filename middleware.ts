7f1425d8710dbd91ce125348ad84782afa5cc4ed
Armeth <jwpro1984@gmail.com>
2024-04-04 17:59:36 -0400

    Forms and things

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // See https://clerk.com/docs/references/nextjs/auth-middleware
 // for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/api/webhooks/clerk"]
+  publicRoutes: ["/", "/api/webhooks/clerk"]
   // Allow signed out users to access the specified routes:
   // publicRoutes: ['/anyone-can-visit-this-route'],
 });
